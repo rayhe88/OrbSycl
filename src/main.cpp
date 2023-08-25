97a3ee65cbbd99f5e560fc2b601c894f8bc59d13
RayHE <rayhe88@gmail.com>
2023-08-25 15:32:21 -0500

    Add feature: Implement a class timer and use it in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include "Evaluator.hpp"
 #include "runCommands.hpp"
+#include "Timer.hpp"
+#include <iostream>
 #include <cstdlib>
 
 int main(int argc, char *argv[])
 {
 
     runCommands run(argc, argv);
+    Timer timer;
 
     Evaluator evaluator(run.npoints, run.r0);
-
+    timer.start();
     evaluator.evaluate_sycl(run.orb);
+    timer.stop();
 
+    std::cout << " Duration: " << timer.getDuration() << " us" << std::endl;
     evaluator.dumpCube(run.name);
 
     exit(EXIT_SUCCESS);
